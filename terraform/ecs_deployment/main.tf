399e38e21fc3061bba74e3f68de86ee371cd2632
Ignacy Sawa <bd1cb5c6@opayq.com>
2024-07-11 22:47:32 +0200

    Added task definition to the main.tf
    Also deleted the separate file fot the task definition.
    Had to also define an IAM role fot the task, because task needs
    permissions to use parts of AWS API to manage the containers.

--- terraform/ecs_deployment/main.tf
+++ terraform/ecs_deployment/main.tf
@@ -1,14 +1,91 @@
+# Pin dependencies to silence the linter
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.57.0"
+    }
+  }
+}
+
+/* Provider Definition */
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ecr_repository" "flask_cicd_repo" {
+# Needed to fetch the account ID
+data "aws_caller_identity" "current" {}
+
+# Declare the ECR repository we are going to use
+data "aws_ecr_repository" "ecr_repo" {
   # ECR repository defined in another Terraform file
-  name   = "flask-cicd"
+  name = var.ecr_repo_name
 }
 
-resource "aws_ecs_cluster" "flask_cicd_ecs_cluster" {
-  name   = "flask-cicd-ecs-cluster"
+# Define an ECS cluster for our app
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.ecs_cluster_name
 }
 
+# Declare task definition for our deployment
+resource "aws_ecs_task_definition" "flask_task" {
+  family = var.task_family_name
+  # operatingSystemFamily required when using Fargate
+  runtime_platform { operating_system_family = "LINUX" }
+  # awsvpc is the only option for Fargate
+  network_mode = "awsvpc"
+  # To use Fargate as the launch type
+  requires_compatibilities = ["FARGATE"]
 
+  # Hardware requirements
+  cpu    = var.container_resources.cpu
+  memory = var.container_resources.ram
+
+  # ARN of the IAM role to allow the ECS Task call AWS API
+  execution_role_arn = aws_iam_role.ecsTaskExecutionRole.arn
+  # Used container
+  container_definitions = jsonencode([{
+    name = var.task_container_name
+    # image - that's the form we use when running containers using
+    # the docker command
+    #"image": ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${REPO}
+    image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}"
+    # TODO: Find a way to split this string into multiple lines.
+    cpu       = var.container_resources.cpu
+    memory    = var.container_resources.ram
+    essential = true
+    portMappings = [{
+      containerPort = var.flask_http_port
+      hostPort      = var.flask_http_port
+      protocol      = "tcp"
+    }]
+    environment = [{
+      name  = "FLASK_ENV"
+      value = var.flask_environment_setting
+    }]
+  }])
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = var.task_execution_role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = (
+  "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
+}
--- terraform/ecs_deployment/vars.tf
+++ terraform/ecs_deployment/vars.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "vpc_cidr_block" {
   description = "CIDR range/block for the main VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.1.0.0/16"
 }
 
 variable "vpc_name" {
@@ -21,7 +21,7 @@ variable "vpc_name" {
 variable "public_subnet_cidr_block" {
   description = "CIDR block for the public subnet"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.1.1.0/24"
 }
 
 variable "is_public_subnet_public" {
@@ -33,13 +33,13 @@ variable "is_public_subnet_public" {
 variable "public_subnet_name" {
   description = "Name assigned to the public subnet"
   type        = string
-  default     = "JenkinsSubnet"
+  default     = "FlaskSubnet"
 }
 
 variable "internet_gateway_name" {
   description = "Name assigned to the internet gateway"
   type        = string
-  default     = "JenkinsIGW"
+  default     = "FlaskIGW"
 }
 
 variable "public_subnet_routing_table_name" {
@@ -49,9 +49,9 @@ variable "public_subnet_routing_table_name" {
 }
 
 variable "security_group_name" {
-  description = "Name assigned to the Jenkin's security group"
+  description = "Name assigned to the Flask security group"
   type        = string
-  default     = "JenkinsSG"
+  default     = "FlaskSG"
 }
 
 /* App related variables */
@@ -68,8 +68,57 @@ variable "flask_http_port" {
 # }
 
 /* ECS/Fargate related variables */
-variable "" {
-  description = ""
-  type        = 
-  default     =
+variable "ecr_repo_name" {
+  description = "Name of the ECR repo"
+  type        = string
+  default     = "flask-cicd"
+}
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "flask-ecs-cluster"
+}
+
+
+variable "task_family_name" {
+  description = "Name of the family of the app task"
+  type        = string
+  default     = "flask_from_jenkins"
+}
+
+variable "container_resources" {
+  type = object({
+    cpu = number # CPU units where 1024 equals 1 vCPU
+    ram = number # Ram in MB
+  })
+  description = (
+  "Resources required for the container in CPU units and RAM")
+  default = {
+    cpu = 256 # 0.25 vCPU
+    ram = 512 # 512 MB
+  }
+}
+
+variable "task_container_name" {
+  description = "Docker name of the container (not image)"
+  type        = string
+  default     = "flask_container"
+}
+
+variable "flask_environment_setting" {
+  description = <<-EOF
+    Container environment variable that chooses flask environment setting.
+    Possible options are: development, production and testing.
+    This choice has security implications.
+  EOF
+  type        = string
+  default     = "production" # Using production to enhance security
+}
+
+variable "task_execution_role_name" {
+  description = (
+  "Name of the IAM role that gives the task execution privileges.")
+  type    = string
+  default = "ecsTaskExecutionRole"
 }
